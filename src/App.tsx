30b4931f32c91cf7ffd2bc13f4f6e9bb81af5590
Daniel Castillo <dlcastillo3015@gmail.com>
2024-03-03 14:12:22 -0500

    feat: add useTitle for js #4

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
-import { useTitle } from "./hooks/ts/browserTab/useTitle";
-import { useEffect } from "react";
-
 function App() {
-  const { title, changeTitle } = useTitle();
-
-  useEffect(() => {
-    console.log(title);
-  }, [title]);
-
-  return (
-    <>
-      <button onClick={() => changeTitle(Math.random().toString())}>
-        Change title
-      </button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
