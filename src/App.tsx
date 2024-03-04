6d3699a2feca7158f11b63dbec444496fa7b885f
Daniel Castillo <dlcastillo3015@gmail.com>
2024-03-04 07:22:34 -0500

    feat: useFavicon for js #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import { useFavicon } from "./hooks/ts/browserTab/useFavicon";
-import fav1 from "./favicon1.ico";
-import fav2 from "./favicon2.ico";
-
 function App() {
-  const changeFavicon = useFavicon();
-
-  return (
-    <>
-      <button onClick={() => changeFavicon(fav1)}>Change 1</button>
-      <button onClick={() => changeFavicon(fav2)}>Change 2</button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
