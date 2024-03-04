1c3aba529d995741d6c5b8177a30956a790a1393
Daniel Castillo <dlcastillo3015@gmail.com>
2024-03-04 07:19:58 -0500

    feat: useFavicon for ts #5

---
+++ src/hooks/ts/browserTab/useFavicon.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export const useFavicon = (): ((newFavicon: string) => void) => {
+  const [faviconUrl, setFaviconUrl] = useState<string>("");
+
+  useEffect(() => {
+    let link = document.querySelector(`link[rel~="icon"]`) as HTMLLinkElement;
+
+    if (!link) {
+      link = document.createElement("link");
+      link.type = "image/x-icon";
+      link.rel = "icon";
+      document.head.appendChild(link);
+    }
+
+    link.href = faviconUrl;
+  }, [faviconUrl]);
+
+  const changeFavicon = (newFavicon: string) => setFaviconUrl(newFavicon);
+
+  return changeFavicon;
+};
