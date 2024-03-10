28687748271ae8f1174799c74bcd80449452bb7a
Daniel Castillo <dlcastillo3015@gmail.com>
2024-03-10 08:51:05 -0400

    add useClipboard for ts #12

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { useCountdown } from "./hooks/js/useCountdown";
+import { useClipboard } from "./hooks/ts/useClipboard";
+import { useRef } from "react";
 
 function App() {
-  const { current, isPaused, isOver, pause, play, reset, togglePause } =
-    useCountdown(0, 10);
+  const { copiedText, copyToClipboard } = useClipboard();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    copyToClipboard(inputRef.current?.value || "")
+      .then(() => alert("Copied!"))
+      .catch(() => alert("Failed!"));
+  };
 
   return (
     <div>
-      <p>Counter value: {current}</p>
-      <p>Is the counter paused? {isPaused ? "Yes" : "No"}</p>
-      <p>Has the counter over? {isOver ? "Yes" : "No"}</p>
-      <button onClick={pause}>Pause</button>
-      <button onClick={play}>Play</button>
-      <button onClick={reset}>Reset</button>
-      <button onClick={togglePause}>Toggle Pause</button>
+      <input type="text" ref={inputRef} />
+      <button onClick={handleClick}>Copy input text</button>
     </div>
   );
 }
