4e13b8d9304ac3e7d4e80affacb6a6518549a943
Daniel Castillo <dlcastillo3015@gmail.com>
2024-03-10 09:34:57 -0400

    feat: add useCountup for ts #14

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import { useClipboard } from "./hooks/ts/useClipboard";
-import { useRef } from "react";
+import { useCountup } from "./hooks/ts/useCountup";
 
 function App() {
-  const { copiedText, copyToClipboard } = useClipboard();
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const handleClick = () => {
-    copyToClipboard(inputRef.current?.value || "")
-      .then(() => alert("Copied!"))
-      .catch(() => alert("Failed!"));
-  };
+  const { current, isPaused, isOver, pause, play, reset, togglePause } =
+    useCountup(0, 3);
 
   return (
     <div>
-      <input type="text" ref={inputRef} />
-      <button onClick={handleClick}>Copy input text</button>
+      <p>Counter value: {current}</p>
+      <p>Is the counter paused? {isPaused ? "Yes" : "No"}</p>
+      <p>Has the counter over? {isOver ? "Yes" : "No"}</p>
+      <button onClick={pause}>Pause</button>
+      <button onClick={play}>Play</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={togglePause}>Toggle Pause</button>
     </div>
   );
 }
