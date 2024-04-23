eef0ff3fb9b73eb809a54acd35fde4e8dcdd8eca
Sergio Villa <sergiovv0110@gmail.com>
2024-04-23 16:56:13 -0700

    feat: add useIsTouchDevice for js & ts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useBattery } from "./hooks/js/useBattery";
+import { useIsTouchDevice } from "./hooks/js/useIsTouchDevice"
 
 function AppJs() {
   const battery = useBattery();
+  const isTouchDevice = useIsTouchDevice();
 
   return (
     <div>
       <p>Battery level:{battery.level * 100}</p>
       <p>{battery.charging ? "Battery charging" : "Battery not charging"}</p>
+
+      {isTouchDevice ? 'It is a touch device' : 'It is not a touch device'}
+
     </div>
   );
 }
