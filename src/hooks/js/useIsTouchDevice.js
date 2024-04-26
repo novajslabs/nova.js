fc834a04749e858305b51cba5f33b201c3ac2ab6
Daniel Castillo <dlcastillo3015@gmail.com>
2024-04-26 10:29:59 -0400

    fix: typo on useIsTouchDevice #47

--- src/hooks/js/useIsTouchDevice.js
+++ src/hooks/js/useIsTouchDevice.js
@@ -1,25 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export function useIsTouchDevice() {
-    const [isTouchDevice, setIsTouchDevice] = useState(false);
-  
-    useEffect(() => {
-      function onResize() {
-        setIsTouchDevice(
-          'ontouchstart' in window ||
+  const [isTouchDevice, setIsTouchDevice] = useState(false);
+
+  useEffect(() => {
+    function onResize() {
+      setIsTouchDevice(
+        "ontouchstart" in window ||
           navigator.maxTouchPoints > 0 ||
-          navigator.msMaxTouchPoints > 0
-        );
-      }
-  
-      window.addEventListener('resize', onResize);
-      onResize();
-  
-      return () => {
-        window.removeEventListener('resize', onResize);
-      };
-    }, []);
-  
-    return isTouchDevice;
-  }
-  
+          navigator.maxTouchPoints > 0
+      );
+    }
+
+    window.addEventListener("resize", onResize);
+    onResize();
+
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+
+  return isTouchDevice;
+}
