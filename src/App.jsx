c949b475c79b833b3fc0adef8023f695ece11d23
Daniel Castillo <dlcastillo3015@gmail.com>
2024-05-01 08:03:53 -0400

    refactor: reset App files #63

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,5 @@
-import { useBattery } from "./hooks/js/useBattery";
-import { useIsTouchDevice } from "./hooks/js/useIsTouchDevice";
-import { useScript } from "./hooks/js/useScript";
-import { useRef } from "react";
-import { useOutsideClick } from "./hooks/js/useOutsideClick";
-
-function AppJs() {
-  const battery = useBattery();
-  const isTouchDevice = useIsTouchDevice();
-  const mermaidIsReady = useScript(
-    "https://cdnjs.cloudflare.com/ajax/libs/mermaid/10.9.0/mermaid.min.js"
-  );
-  const classDiagramExample = `
-            classDiagram
-            class GeoPointType {
-            <<enumeration>>
-              BROWNFIELD
-              OGWELL
-              CELL_TOWER
-              NUCLEAR_REACTOR
-              SUPERFUND
-            }`;
-
-  const testRef = useRef(null);
-  useOutsideClick(testRef, () => console.log("Testing"));
-
-  return (
-    <div ref={testRef}>
-      <p>Battery level:{battery.level * 100}</p>
-      <p>{battery.charging ? "Battery charging" : "Battery not charging"}</p>
-
-      {isTouchDevice ? "It is a touch device" : "It is not a touch device"}
-
-      {mermaidIsReady && (
-        <>
-          <h2>Mermaid</h2>
-          <div className="mermaid">{classDiagramExample}</div>
-        </>
-      )}
-    </div>
-  );
-}
+const AppJs = () => {
+  return <p>Test your hook here</p>;
+};
 
 export default AppJs;
--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,5 @@
-import { useBattery } from "./hooks/ts/useBattery";
-import { useIsTouchDevice } from "./hooks/ts/useIsTouchDevice";
-import { useScript } from "./hooks/ts/useScript";
-
-function AppTs() {
-  const battery = useBattery();
-  const isTouchDevice = useIsTouchDevice();
-  const mermaidIsReady = useScript(
-    "https://cdnjs.cloudflare.com/ajax/libs/mermaid/10.9.0/mermaid.min.js"
-  );
-
-  const classDiagramExample = `
-          classDiagram
-          class GeoPointType {
-          <<enumeration>>
-            BROWNFIELD
-            OGWELL
-            CELL_TOWER
-            NUCLEAR_REACTOR
-            SUPERFUND
-          }`;
-  return (
-    <div>
-      <p>Battery level:{battery.level && battery.level * 100}</p>
-      <p>{battery.charging ? "Battery charging" : "Battery not charging"}</p>
-
-      {isTouchDevice ? "It is a touch device" : "It is not a touch device"}
-
-      {mermaidIsReady && (
-        <>
-          <h2>Mermaid</h2>
-          <div className="mermaid">{classDiagramExample}</div>
-        </>
-      )}
-    </div>
-  );
-}
+const AppTs = () => {
+  return <p>Test your hook here</p>;
+};
 
 export default AppTs;
