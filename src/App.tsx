9673dd8e28a60eb7a7aeefb79fca283860107f4e
Bryan Morales <bryandms95@gmail.com>
2024-05-05 13:25:29 -0600

    chore: remove unused example code

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,5 @@
-import { useDownloadFile } from "./hooks/ts/useDownloadFile";
-
 const AppTs = () => {
-  const { error, isDownloading, downloadFile } = useDownloadFile();
-
-  const onClickSuccessfulDownload = () => {
-    downloadFile(
-      "README.md",
-      "https://raw.githubusercontent.com/novajslabs/nova.js/main/README.md"
-    );
-  };
-
-  const onClickButtonFailedDownload = () => {
-    downloadFile(
-      "novajs.zip",
-      "https://github.com/novajslabs/nova.js/archive/refs/heads/main.zip"
-    );
-  };
-
-  return (
-    <div>
-      <h3>Successful download</h3>
-      <button disabled={isDownloading} onClick={onClickSuccessfulDownload}>
-        {isDownloading ? "Downloading..." : "Download file"}
-      </button>
-
-      <h3>Failed download</h3>
-      <button disabled={isDownloading} onClick={onClickButtonFailedDownload}>
-        {isDownloading ? "Downloading..." : "Download file"}
-      </button>
-
-      {!!error && <p>Could not download file</p>}
-    </div>
-  );
+  return <p>Test your hook here</p>;
 };
 
 export default AppTs;
