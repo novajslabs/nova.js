ef4442e2e5a2f403559df758698a61e63a2d535f
Daniel Castillo <dlcastillo3015@gmail.com>
2024-05-13 15:08:10 -0400

    feat: add useKeyPress for js #25

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import { useKeyPress } from "./hooks/ts/useKeyPress";
-
 const AppTs = () => {
-  const isBPressed = useKeyPress({ key: "b" });
-
-  return <div>{isBPressed ? "b is pressed" : "b is not pressed"}</div>;
+  return <p>Test your hook here</p>;
 };
 
 export default AppTs;
