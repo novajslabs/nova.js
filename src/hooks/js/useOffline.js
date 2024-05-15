0bc923550a7692365e8e5c929b1ce3565c210cdd
alnavarrop99 <anavarro.develop99@gmail.com>
2024-05-15 02:00:00 -0400

    feat(js): create useOffline hook

---
+++ src/hooks/js/useOffline.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react"
+
+export const useOffline = () => {
+  const [offline, setOffline] = useState(null)
+  
+  useEffect( () => {
+    const handleNetworkState = () => {
+      setOffline(!offline)
+    }
+    addEventListener( "offline", handleNetworkState )
+    addEventListener( "online", handleNetworkState )
+
+    return () => {
+      removeEventListener("online", handleNetworkState)
+      removeEventListener("offline", handleNetworkState)
+    }
+  }, [offline] )
+
+  return !!offline
+}
