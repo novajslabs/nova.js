ae231e87cf37047ae357676d92b9d2d5f427773c
Daniel Castillo <dlcastillo3015@gmail.com>
2024-05-19 08:53:30 -0400

    feat: delete all-js-related files

--- src/hooks/js/useToggle.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export const useToggle = (initialValue) => {
-  const [current, setCurrent] = useState(initialValue);
-
-  const handleToggle = () => setCurrent((prev) => !prev);
-
-  return { current, handleToggle };
-};
