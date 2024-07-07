629c4ecdfa41613e80cd04d7ee69c58e6fd6fd1c
Daniel Castillo <dlcastillo3015@gmail.com>
2024-07-07 08:42:48 -0400

    feat: delete unnecesary comments #97

--- src/hooks/js/usePrevious.js
+++ src/hooks/js/usePrevious.js
@@ -1,21 +1,13 @@
-/**
- * usePrevious.js
- *
- * The usePrevious is a custom hook that allows a component to keep track of the previous
- * value of a variable. This is useful when you want to compare the current value of a variable
- * with its previous value.
- */
-
 import { useRef } from 'react';
 
 export default function usePrevious(value) {
-    const currentRef = useRef(value);
-    const previousRef = useRef();
+  const currentRef = useRef(value);
+  const previousRef = useRef();
 
-    if (currentRef.current !== value) {
-        previousRef.current = currentRef.current;
-        currentRef.current = value;
-    }
+  if (currentRef.current !== value) {
+    previousRef.current = currentRef.current;
+    currentRef.current = value;
+  }
 
-    return previousRef.current;
+  return previousRef.current;
 }
