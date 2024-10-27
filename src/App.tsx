06764865c1209d4870c829d72ec5dfcd77b90b73
Daniel Castillo <dlcastillo3015@gmail.com>
2024-10-27 10:20:16 -0400

    feat: remove unnecessary useEffect #118

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { useTitle } from './hooks/ts/useTitle';
+
 const App = () => {
-  return <p>Test your hook here</p>;
+  const { title, changeTitle } = useTitle();
+
+  const toggleTitle = () => {
+    if (title === "Title 1") {
+      changeTitle("Title 2");
+    } else {
+      changeTitle("Title 1");
+    }
+  };
+
+  return <button onClick={toggleTitle}>Toggle title</button>;
 };
 
 export default App;
