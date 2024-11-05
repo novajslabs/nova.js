59fe7d4455c886b0637dd9ce6ff098780e6c71bf
Dayan Perez <dayan.perez1@autodesk.com>
2024-11-05 13:10:01 -0300

    feat: add useMeasure hook to provide a convenient and efficient way to monitor and respond to changes in the size of a React component

---
+++ src/hooks/js/useMeasure.js
@@ -0,0 +1,28 @@
+import { useState, useEffect, useRef } from 'react'
+
+export const useMeasure = () => {
+    const [size, setSize] = useState({ width: 0, height: 0 })
+    const elementRef = useRef(null)
+
+    useEffect(() => {
+        const observer = new ResizeObserver((entries) => {
+            if (entries.length > 0) {
+                const entry = entries[0]
+                const { width, height } = entry.contentRect
+                setSize({ width, height })
+            }
+        })
+
+        if (elementRef.current) {
+            observer.observe(elementRef.current)
+        }
+
+        return () => {
+            if (elementRef.current) {
+                observer.disconnect()
+            }
+        }
+    }, [])
+
+    return [elementRef, size]
+}
