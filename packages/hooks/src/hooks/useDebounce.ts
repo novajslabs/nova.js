7e23b1bd451cbe493025cb59415b569719156d4b
Daniel Castillo <dlcastillo3015@gmail.com>
2026-04-01 11:32:17 -0400

    chore(hooks): name useEffect in useDebounce

--- packages/hooks/src/hooks/useDebounce.ts
+++ packages/hooks/src/hooks/useDebounce.ts
@@ -3,15 +3,16 @@ import { useEffect, useState } from "react";
 export const useDebounce = <T>(value: T, delay: number) => {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
-  useEffect(() => {
-    const handleTimeout = setTimeout(() => {
-      setDebouncedValue(value);
-    }, delay);
+  useEffect(
+    function syncDebouncedValue() {
+      const handleTimeout = setTimeout(() => {
+        setDebouncedValue(value);
+      }, delay);
 
-    return () => {
-      clearTimeout(handleTimeout);
-    };
-  }, [value, delay]);
+      return () => clearTimeout(handleTimeout);
+    },
+    [value, delay],
+  );
 
   return debouncedValue;
 };
