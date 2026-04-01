625b3215111705a8d42b10693021d18a64c8a8fa
Daniel Castillo <dlcastillo3015@gmail.com>
2026-04-01 07:48:07 -0400

    chore(hooks): add jsdoc to useArray

--- packages/hooks/src/hooks/useArray.ts
+++ packages/hooks/src/hooks/useArray.ts
@@ -1,6 +1,28 @@
 import { useState } from "react";
 
-export const useArray = <T>(initialArray: T[]) => {
+/**
+ * React hook to manage and manipulate arrays.
+ *
+ * @param {T[]} initialArray - The array to manage and manipulate.
+ *
+ * @returns {Object} An object with the array state and manipulation methods.
+ * @returns {T[]} array - The current array state.
+ * @returns {React.Dispatch<React.SetStateAction<T[]>>} set - Directly sets the array state.
+ * @returns {(element: T) => void} push - Appends an element to the end of the array.
+ * @returns {(callback: (element: T) => boolean) => void} filter - Filters the array using a callback function.
+ * @returns {(index: number, newElement: T) => void} update - Replaces the element at the given index with a new element.
+ * @returns {(index: number) => void} remove - Removes the element at the given index.
+ * @returns {() => void} clear - Empties the array.
+ *
+ * @example
+ * const { array, push, remove, clear } = useArray<string>([]);
+ *
+ * push("hello");   // ["hello"]
+ * push("world");   // ["hello", "world"]
+ * remove(0);       // ["world"]
+ * clear();         // []
+ */
+export const useArray = <T>(initialArray: T[]): object => {
   const [array, setArray] = useState<T[]>(initialArray);
 
   const push = (element: T) => {
