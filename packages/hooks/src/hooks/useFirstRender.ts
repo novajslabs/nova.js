881d78505f21b63d35d4962d929d1fd8030b770a
Daniel Castillo <dlcastillo3015@gmail.com>
2026-04-10 13:54:11 -0400

    chore(hooks): add jsdoc to useFirstRender

--- packages/hooks/src/hooks/useFirstRender.ts
+++ packages/hooks/src/hooks/useFirstRender.ts
@@ -1,5 +1,10 @@
 import { useRef, useEffect } from "react";
 
+/**
+ * React hook to know whether the current render is the initial render.
+ *
+ * @returns {boolean} `true` only during the first render.
+ */
 export const useFirstRender = () => {
   const firstRender = useRef(true);
 
